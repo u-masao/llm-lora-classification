0390b2d95b901d1e0b722f93fb7f0b3d898e8733
u-masao <4973920+u-masao@users.noreply.github.com>
2025-07-27 23:40:23 +0900

    update run script

--- run_direct.sh
+++ run_direct.sh
@@ -4,22 +4,28 @@ set -x
 
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-150m --max_seq_len 512
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-150m --max_seq_len 4096
-exit
-CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name google/gemma-3-1b-pt
-CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-1.8b
+CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-440m --max_seq_len 4096
+CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-980m --max_seq_len 4096
+CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name Qwen/Qwen3-0.6B --max_seq_len 4096
+CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name Qwen/Qwen3-1.7B
+CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name google/gemma-3-1b-pt --max_seq_len 4096
 exit
 
+# trouble
 # CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name microsoft/Phi-4-mini-instruct
 # CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name openai-community/gpt2
 # CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-modernbert-base
 
+# Qwen
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name Qwen/Qwen3-0.6B
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name Qwen/Qwen3-1.7B
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name Qwen/Qwen3-4B
 
+# llm-jp
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-150m
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-440m
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-980m
+CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3-1.8b
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name llm-jp/llm-jp-3.1-1.8b
 
 CUDA_VISIBLE_DEVICES=0 uv run python src/train.py --model_name cyberagent/open-calm-1b
